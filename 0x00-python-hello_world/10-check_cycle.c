bf54f0153befa900bb040888483b8d1bcd929a9e
eboayue <keboayue@gmail.com>
2018-09-11 04:14:28 +0000

    Add all files for all tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the given list cycles back on itself
+ * @list: the given list
+ *
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *node1, *node2;
+
+	if (list == NULL || list->next == NULL || list->next == list)
+		return (0);
+
+	node1 = list;
+	node2 = node1->next;
+
+	while (node2->next != NULL)
+	{
+		if (node1 == node2)
+			return (1);
+		node1 = node1->next;
+		node2 = node2->next->next;
+	}
+	return (0);
+}
