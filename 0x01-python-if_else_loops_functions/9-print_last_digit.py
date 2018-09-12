eb0886a32c0484884db3317ede3a1518c67e998a
eboayue <keboayue@gmail.com>
2018-09-12 17:04:08 +0000

    Add files for task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    """Prints the last digit of a number"""
+    if number < 0:
+        number *= -1
+        print("{}".format(number % 10), end="")
+    else:
+        print("{}".format(number % 10), end="")
+    return number % 10
