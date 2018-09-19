8a03d920a65b1d5b6c23070eb68cf9877dcf22b1
eboayue <keboayue@gmail.com>
2018-09-19 03:25:04 +0000

    add format specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j is not i[len(i) - 1]:
-                print("{} ".format(j), end="")
+                print("{:d} ".format(j), end="")
             else:
-                print ("{}".format(j), end="")
+                print ("{:d}".format(j), end="")
         print()
