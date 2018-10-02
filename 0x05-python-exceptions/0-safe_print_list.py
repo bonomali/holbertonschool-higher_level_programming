c792506021ca5b067360a8481cd8d9602aff3893
eboayue <keboayue@gmail.com>
2018-10-02 03:25:39 +0000

    Account for empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """Prints x elements of a list"""
+    i = 0
     if my_list:
-        for i in range(x):
+        for j in range(x):
             try:
                 print("{}".format(my_list[i]), end="")
             except IndexError:
