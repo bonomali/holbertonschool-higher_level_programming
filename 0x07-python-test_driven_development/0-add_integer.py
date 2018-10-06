3cf6885bd0764386e254376b5d8a957123b1d3cb
eboayue <keboayue@gmail.com>
2018-10-06 18:16:19 +0000

    Fix error message output

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def add_integer(a, b=98):
     """Adds two integers.
     Args:
@@ -6,7 +6,7 @@ def add_integer(a, b=98):
     b (int): second number
     """
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an int")
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an int")
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
