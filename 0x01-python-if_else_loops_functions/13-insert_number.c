36a001aa1ac19a9103b79c6748e4954cac4392e3
Essence Boayue <essenceboayue@Essences-MacBook-Pro.local>
2018-10-15 13:11:21 -0700

    Unifinished task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,10 @@
+/**
+ * insert_node - inserts a number into a sorted list
+ * @head: pointer to head of the list
+ * @number: the given number
+ *
+ * Return: the address of the new node, or NULL on failure
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	
