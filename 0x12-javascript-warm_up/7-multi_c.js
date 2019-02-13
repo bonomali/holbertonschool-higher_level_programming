34fec2f6f892402963925ec13369397040909d18
eboayue <keboayue@gmail.com>
2019-02-13 00:01:45 -0800

    Finsh task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Prints "c is fun" x number of times
+
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < x; i++){
+  console.log('C is fun');
+}
