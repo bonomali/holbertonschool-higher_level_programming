2aff4b15fc9924bebeee89a4062af51ac72a36a8
eboayue <keboayue@gmail.com>
2019-02-19 17:22:07 -0800

    Get rid of newline

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // Writes a string to file
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3] + '\n', 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
